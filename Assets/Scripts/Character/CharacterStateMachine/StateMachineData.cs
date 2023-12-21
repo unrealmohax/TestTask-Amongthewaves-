3b23bacbd54b9b034d1baeeedf0df93d915570a0
unrealmohax <100801007+unrealmohax@users.noreply.github.com>
2023-12-21 20:38:07 +0300

    Правки

--- Assets/Scripts/Character/CharacterStateMachine/StateMachineData.cs
+++ Assets/Scripts/Character/CharacterStateMachine/StateMachineData.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using UnityEngine;
 
 public class StateMachineData
 {
@@ -41,22 +39,4 @@ public class StateMachineData
 
     private float _health;
     private float _speed;
-    private CoroutineTask _changeSpeed;
-
-    /*private IEnumerator ChangeSpeed(float v_end, float duration)
-    {
-        float elapsed = 0.0f;
-        float v_start = _currspeed;
-
-        while (elapsed < duration)
-        {
-            elapsed += Time.deltaTime;
-            _currspeed = Mathf.Lerp(v_start, v_end, elapsed / duration); 
-            yield return null;
-        }
-
-        _currspeed = v_end;
-        _changeSpeed = null;
-    }*/
-
 }
